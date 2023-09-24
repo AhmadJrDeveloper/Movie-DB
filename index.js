0e6d74ceab876c4003e662847cc584099366f5f5
ahmadjrdeveloper <ahmadsaad200235@gmail.com>
2023-09-24 20:29:31 +0300

    step 3

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ const app = express();
 app.get('/',(req,res)=>{
     res.send("ok");
 })
+app.get('/test',(req,res)=>{
+    res.status(200).json({status:200, message:"ok"});
+})
+app.get('/time',(req,res)=>{
+    const date = new Date();
+    const hours = date.getHours().toString()
+    const minute = date.getMinutes().toString()
+    const time = `${hours}:${minute}`;
+    res.status(200).json({status:200, message:time})
+})
 app.listen(3000);
