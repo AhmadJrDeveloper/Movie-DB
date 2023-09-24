0a6b43f86508ba13445ecc8fa795154ca82ca774
ahmadjrdeveloper <ahmadsaad200235@gmail.com>
2023-09-24 21:24:13 +0300

    step 4

--- index.js
+++ index.js
@@ -14,4 +14,19 @@ app.get('/time',(req,res)=>{
     const time = `${hours}:${minute}`;
     res.status(200).json({status:200, message:time})
 })
+app.get('/hello/:id?',(req,res)=>{
+    const {id} = req.params;
+    if(id)
+        res.status(200).json({status:200, message:"hello", id});
+    else
+        res.status(200).json({status:200, message:"hello"});
+
+})
+app.get('/search',(req,res)=>{
+    const search = req.query.s;
+    if(search)
+        res.status(200).json({status:200, message:"ok", data:search})
+    else
+    res.status(500).json({status:500, error:true, message:"you have to provide a search"});
+})
 app.listen(3000);
